4fcaaeeee3ec154206c1fe5b210e51c7c83ab6c0
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 17:01:02 +0800

    对DTOs\Responses添加注释

--- Models/DTOs/Responses/RegisterResult.cs
+++ Models/DTOs/Responses/RegisterResult.cs
@@ -1,5 +1,8 @@
 namespace WebApplication1.Models.DTOs.Responses;
-
+/***
+ * 这是一个用于注册结果的DTO类，包含了用户的OpenId、昵称、头像URL、注册是否成功、
+ * 消息以及用户ID等属性。它可以用于返回前端注册操作的结果信息。
+ */
 public class RegisterResult
 {
     public string OpenId { get; set; } = string.Empty;
