24f2c41d419f95d003a7ffa96018851542287d53
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 16:59:56 +0800

    对DTOs\Request添加注释

--- Models/DTOs/Requests/ApproveUserRequest.cs
+++ Models/DTOs/Requests/ApproveUserRequest.cs
@@ -1,4 +1,5 @@
-﻿namespace WebApplication1.Models.DTOs.Requests
+﻿//这是一个用于批准用户请求的DTO类，包含一个布尔属性Approve表示是否批准，以及一个可选的字符串属性Note用于添加备注信息。
+namespace WebApplication1.Models.DTOs.Requests
 {
     public class ApproveUserRequest
     {
