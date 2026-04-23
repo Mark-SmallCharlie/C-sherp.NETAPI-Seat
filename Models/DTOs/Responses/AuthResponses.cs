dbd77f1dd02209dc04038b6351b775b60fdbbae5
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 17:00:53 +0800

    对DTOs\Responses添加注释

--- Models/DTOs/Responses/AuthResponses.cs
+++ Models/DTOs/Responses/AuthResponses.cs
@@ -1,4 +1,7 @@
-﻿using WebApplication1.Models.DTOs.Requests;
+﻿/***
+ * 这是一个用于管理员用户响应的DTO类，包含用户的ID、用户名、显示名称、是否激活以及创建时间等属性。
+ ***/
+using WebApplication1.Models.DTOs.Requests;
 using WebApplication1.Models.DTOs.Responses;
 
 namespace WebApplication1.Models.DTOs.Responses;
