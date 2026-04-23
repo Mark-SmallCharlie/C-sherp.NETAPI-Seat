f9747f3f3d9f437eb73f07ec55837d3dea6b06b1
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 17:00:31 +0800

    对DTOs\Responses添加注释

--- Models/DTOs/Responses/ApiResponse.cs
+++ Models/DTOs/Responses/ApiResponse.cs
@@ -1,4 +1,7 @@
-﻿namespace WebApplication1.Models.DTOs.Responses
+﻿/***这是一个用于API响应的类，包含了一个泛型版本和一个非泛型版本。泛型版本可以返回数据，
+ * 而非泛型版本则适用于不需要返回数据的操作。每个类都包含一个表示操作是否成功的布尔值和一个消息字符串。
+***/
+namespace WebApplication1.Models.DTOs.Responses
 {
     // 泛型响应类，用于返回数据
     public class ApiResponse<T>
