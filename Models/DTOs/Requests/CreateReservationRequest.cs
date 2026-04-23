d36fce93cba55301de0b49042647e42d6151ba50
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 17:00:01 +0800

    对DTOs\Request添加注释

--- Models/DTOs/Requests/CreateReservationRequest.cs
+++ Models/DTOs/Requests/CreateReservationRequest.cs
@@ -1,4 +1,5 @@
-﻿namespace WebApplication1.Models.DTOs.Requests
+﻿// 这是一个用于创建预订请求的DTO类，包含座位号、开始时间、结束时间以及用户的认证信息（用户名、密码和显示名称）。
+namespace WebApplication1.Models.DTOs.Requests
 {
     public class CreateReservationRequest
     {
