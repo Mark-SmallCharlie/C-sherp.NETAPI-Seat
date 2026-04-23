578291fe55ff6efcd23baad3e3589d7bb282b1b6
Mark-SmallCharlie <3151899431@qq.com>
2026-04-23 17:01:06 +0800

    对DTOs\Responses添加注释

--- Models/DTOs/Responses/UserInfoResponse.cs
+++ Models/DTOs/Responses/UserInfoResponse.cs
@@ -1,4 +1,8 @@
-﻿namespace WebApplication1.Models.DTOs.Responses
+﻿/**
+ * 这是一个用于封装用户信息的响应DTO类，包含了用户的基本信息，如ID、昵称、头像URL、角色和显示名称等属性。
+ * 这个类可以用于在API响应中返回用户信息，特别是在管理员界面中显示用户的详细信息。
+ */
+namespace WebApplication1.Models.DTOs.Responses
 {
     public class UserInfoResponse
     {
