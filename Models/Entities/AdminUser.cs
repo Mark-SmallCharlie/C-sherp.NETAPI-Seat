6871e385a8bbd2a55751ea3b1c0fc65340e1798a
Mark-SmallCharlie <3151899431@qq.com>
2026-04-26 11:50:35 +0800

    添加Models下的Entities文件的类

--- Models/Entities/AdminUser.cs
+++ Models/Entities/AdminUser.cs
@@ -1,4 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+/**
+ * AdminUser实体类定义了管理员用户的数据结构，包含了Id、Username、PasswordHash、DisplayName、IsActive和CreatedAt等属性。
+ * 这些属性分别表示管理员用户的唯一标识、登录用户名、哈希后的密码、显示名称、是否激活以及创建时间。
+ * 通过这个类，开发者可以方便地在数据库中存储和管理管理员用户的信息。
+ */
 namespace WebApplication1.Models.Entities
 {
     public class AdminUser
