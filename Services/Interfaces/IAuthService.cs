82dea8edb302dc6916b3b594a1d876f768b4e052
Mark-SmallCharlie <3151899431@qq.com>
2026-04-29 16:02:20 +0800

    添加了Serives里的接口注释

--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,6 +1,11 @@
 ﻿using WebApplication1.Models.DTOs.Requests;
 using WebApplication1.Models.DTOs.Responses;
-
+/**
+ * 这是一个用于定义认证服务接口的代码文件。
+ * IAuthService接口包含了管理员登录、用户密码登录、微信登录以及生成JWT令牌的方法。
+ * 这些方法分别用于处理不同类型的登录请求，并生成相应的登录响应或JWT令牌。
+ * 通过实现这个接口，开发者可以在应用程序中提供多种认证方式，以满足不同用户的需求。
+ */
 namespace WebApplication1.Services.Interfaces;
 
 public interface IAuthService
