9cf42b761ffa5ee5a776e3475c1ece537a0303ce
Mark-SmallCharlie <3151899431@qq.com>
2026-04-29 16:02:25 +0800

    添加了Serives里的接口注释

--- Services/Interfaces/IDeviceStatusService.cs
+++ Services/Interfaces/IDeviceStatusService.cs
@@ -1,6 +1,7 @@
 ﻿using WebApplication1.Models.Device;
 //using WebApplication1.Models.Device;
-
+// 此接口定义了设备状态服务的功能，
+// 包括更新设备状态、获取设备状态、获取所有设备状态、获取座位占用状态以及配置和移除设备-座位映射等方法。
 namespace WebApplication1.Services.Interfaces
 {
     public interface IDeviceStatusService
