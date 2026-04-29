d6740b5a5d0229274642cc9bab5ba9bcc3578551
Mark-SmallCharlie <3151899431@qq.com>
2026-04-29 16:02:49 +0800

    添加了Serives里的接口注释（mqtt信息）

--- Services/Interfaces/IMqttMessageHandler.cs
+++ Services/Interfaces/IMqttMessageHandler.cs
@@ -1,4 +1,5 @@
-﻿namespace WebApplication1.Services.Interfaces
+﻿// 此接口定义了一个处理MQTT消息的异步方法，接受主题和负载作为参数。实现这个接口的类可以根据需要处理不同的MQTT消息，例如将消息存储到数据库、触发其他服务等。
+namespace WebApplication1.Services.Interfaces
 {
     public interface IMqttMessageHandler
     {
